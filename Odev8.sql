397baf88a415a39e232724f6c44f80cfae1da9a4
Ömer Can Koca <omer.c.koca@gmail>
2024-08-18 13:34:21 +0300

    Ödev 8 eklendi

---
+++ Odev8.sql
@@ -0,0 +1,85 @@
+CREATE TABLE test (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(50) NOT NULL,
+	birthday DATE,
+	email VARCHAR(100)
+);
+
+insert into test (name, birthday, email) values ('Ingar Skyppe', '1968-09-04', 'iskyppe0@macromedia.com');
+insert into test (name, birthday, email) values ('Gabriella Leafe', '1958-03-14', 'gleafe1@woothemes.com');
+insert into test (name, birthday, email) values ('Cornall Mallison', '1995-04-07', 'cmallison2@linkedin.com');
+insert into test (name, birthday, email) values ('Gwenny Lusgdin', '1975-11-03', 'glusgdin3@epa.gov');
+insert into test (name, birthday, email) values ('Joannes Boxill', '1980-03-03', 'jboxill4@nasa.gov');
+insert into test (name, birthday, email) values ('Quinta Campbell', '1991-01-15', 'qcampbell5@vkontakte.ru');
+insert into test (name, birthday, email) values ('Lilith Borthwick', '1969-01-10', 'lborthwick6@is.gd');
+insert into test (name, birthday, email) values ('Kathleen Lesser', '1989-04-01', 'klesser7@live.com');
+insert into test (name, birthday, email) values ('Ashlie Kitchener', '1969-10-30', 'akitchener8@sogou.com');
+insert into test (name, birthday, email) values ('Shepard Hurl', '1950-10-28', 'shurl9@mac.com');
+insert into test (name, birthday, email) values ('Asa Buscher', '1958-08-15', 'abuschera@jimdo.com');
+insert into test (name, birthday, email) values ('Barton Agates', '1975-01-01', 'bagatesb@youtu.be');
+insert into test (name, birthday, email) values ('Farrell MacCallion', '1963-05-04', 'fmaccallionc@noaa.gov');
+insert into test (name, birthday, email) values ('Morly Moden', '1962-05-05', 'mmodend@mysql.com');
+insert into test (name, birthday, email) values ('Heddi Hesey', '1963-02-23', 'hheseye@shop-pro.jp');
+insert into test (name, birthday, email) values ('Karisa O''Carran', '1974-06-02', 'kocarranf@answers.com');
+insert into test (name, birthday, email) values ('Malena Philippard', '1954-11-07', 'mphilippardg@123-reg.co.uk');
+insert into test (name, birthday, email) values ('Harv Benedito', '1975-08-25', 'hbeneditoh@prnewswire.com');
+insert into test (name, birthday, email) values ('Elna Colvine', '2000-05-06', 'ecolvinei@canalblog.com');
+insert into test (name, birthday, email) values ('Corabelle Pascho', '1960-01-22', 'cpaschoj@washingtonpost.com');
+insert into test (name, birthday, email) values ('Allie Linner', '1977-03-29', 'alinnerk@abc.net.au');
+insert into test (name, birthday, email) values ('Adorne Spir', '1953-04-30', 'aspirl@wiley.com');
+insert into test (name, birthday, email) values ('Ingrid Mulcahy', '1958-08-13', 'imulcahym@msu.edu');
+insert into test (name, birthday, email) values ('Dorella Akroyd', '1976-05-02', 'dakroydn@google.com.hk');
+insert into test (name, birthday, email) values ('Devora Braxay', '1990-12-28', 'dbraxayo@illinois.edu');
+insert into test (name, birthday, email) values ('Silvan Geratasch', '1972-11-15', 'sgerataschp@lycos.com');
+insert into test (name, birthday, email) values ('Huntlee Pycock', '1978-10-29', 'hpycockq@google.cn');
+insert into test (name, birthday, email) values ('Errol Bickley', '1976-05-30', 'ebickleyr@w3.org');
+insert into test (name, birthday, email) values ('Emlynn Reeman', '1980-02-17', 'ereemans@pen.io');
+insert into test (name, birthday, email) values ('Domeniga Hotson', '1986-10-04', 'dhotsont@nba.com');
+insert into test (name, birthday, email) values ('Ranna Matusovsky', '1961-01-20', 'rmatusovskyu@cnet.com');
+insert into test (name, birthday, email) values ('Aigneis Domenge', '1959-01-30', 'adomengev@mapy.cz');
+insert into test (name, birthday, email) values ('Moishe O''Scollee', '1977-04-27', 'moscolleew@flavors.me');
+insert into test (name, birthday, email) values ('Cynde Klehn', '2001-08-25', 'cklehnx@mapy.cz');
+insert into test (name, birthday, email) values ('Benedetto Smithin', '1991-07-07', 'bsmithiny@themeforest.net');
+insert into test (name, birthday, email) values ('Mara Predohl', '1983-01-21', 'mpredohlz@g.co');
+insert into test (name, birthday, email) values ('Ariel Henrionot', '2000-06-15', 'ahenrionot10@ucla.edu');
+insert into test (name, birthday, email) values ('Gilberta Thake', '1950-10-06', 'gthake11@ocn.ne.jp');
+insert into test (name, birthday, email) values ('Herc Bendare', '1979-08-06', 'hbendare12@vkontakte.ru');
+insert into test (name, birthday, email) values ('Eva Steart', '1969-04-11', 'esteart13@angelfire.com');
+insert into test (name, birthday, email) values ('Benedick Staff', '1953-02-28', 'bstaff14@psu.edu');
+insert into test (name, birthday, email) values ('Hally Mattiazzi', '1962-06-19', 'hmattiazzi15@amazon.de');
+insert into test (name, birthday, email) values ('Shalna Cornford', '1988-01-03', 'scornford16@cargocollective.com');
+insert into test (name, birthday, email) values ('Hedwiga Braidon', '1978-10-01', 'hbraidon17@forbes.com');
+insert into test (name, birthday, email) values ('Roxie Barlas', '1962-03-13', 'rbarlas18@dailymotion.com');
+insert into test (name, birthday, email) values ('Jacinta Broad', '1984-05-28', 'jbroad19@amazon.de');
+insert into test (name, birthday, email) values ('Hardy Gummoe', '1998-10-26', 'hgummoe1a@businesswire.com');
+insert into test (name, birthday, email) values ('Fraze Scrivinor', '1954-01-14', 'fscrivinor1b@usda.gov');
+insert into test (name, birthday, email) values ('Vincenz Gashion', '1987-01-07', 'vgashion1c@salon.com');
+insert into test (name, birthday, email) values ('Brady Jurgensen', '1992-03-21', 'bjurgensen1d@netvibes.com');
+
+UPDATE test
+SET name = 'test',
+	birthday = '1998-01-30',
+	email = 'test@test.com'
+WHERE id < 10;
+
+UPDATE test
+SET birthday = '1998-01-30',
+	email = 'testt@testt.com'
+WHERE name LIKE '%a%';
+
+UPDATE test
+SET name = 'test',
+	email = 'test@test.com'
+WHERE birthday = '1978-10-29';
+
+DELETE FROM test
+WHERE id = 28;
+
+DELETE FROM test
+WHERE name = 'Cynde Klehn';
+
+DELETE FROM test
+WHERE birthday = '1991-07-07';
+
+DELETE FROM test
+WHERE email = 'ahenrionot10@ucla.edu';
